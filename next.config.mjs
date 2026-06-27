9174e9d3e9adb4934228ca3695b9b791db40166d
donaldguerrero10-sys <donald.guerrero10@gmail.com>
2026-06-26 23:12:40 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/**": ["./.next/server/app/**/*.js"]
+    }
+  },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '*.supabase.co' },
+      { protocol: "https", hostname: "*.supabase.co" },
     ],
   },
 }
